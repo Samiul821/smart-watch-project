247209504f3e24b99d66aeab26ec40dd6e7550ea
Samiul821 <smsamiul821@gmail.com>
2025-03-03 15:06:43 +0600

    images section function added

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const ringButtons = document.querySelectorAll(".ring-button");
+
+for (let i = 0; i < ringButtons.length; i++) {
+  const ringBtn = ringButtons[i];
+  ringBtn.addEventListener("click", function (event) {
+    const color = event.target.id.replace("-color", "");
+    console.log(color);
+    //    purple add korar age sob guloke check kore purple thakle remove kore dibo abong onno ekta border add kore dibo
+    for (let j = 0; j < ringButtons.length; j++) {
+      ringButtons[j].classList.remove("border-purple-600");
+      ringButtons[j].classList.add("border-gray-300");
+    }
+
+    // color add korteci
+    event.target.classList.add("border-purple-600");
+    event.target.classList.remove("border-gray-300");
+
+    const productImage = document.getElementById("product-image");
+    // productImage.src = "../images/teal.png";
+    productImage.src = "../images/" + color + ".png"
+  });
+}
